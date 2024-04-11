e8d8208d99ee2baf37b4d8a996365f435788f094
Nils <31704359+mietzen@users.noreply.github.com>
2024-04-11 06:37:12 +0200

    Update virtualenv.plugin.zsh

--- virtualenv.plugin.zsh
+++ virtualenv.plugin.zsh
@@ -1,6 +1,7 @@
 function virtualenv_prompt_info(){
   [[ -n ${VIRTUAL_ENV} ]] || return
   VIRTUAL_ENV_PROMPT=$(cat $VIRTUAL_ENV/pyvenv.cfg | grep '^prompt' | awk -F "'" '{print $2}' | xargs)
+  [[ -n ${VIRTUAL_ENV_PROMPT} ]] || return
   echo "${ZSH_THEME_VIRTUALENV_PREFIX=[}$VIRTUAL_ENV_PROMPT${ZSH_THEME_VIRTUALENV_SUFFIX=]}"
 }
 
